cb223363a3244f5b73ae003cfb83d502b46ad291
Tianna <75389500+UselessToaster@users.noreply.github.com>
2024-02-16 15:46:27 -0500

    Update DCP-546.py
    Optimized code after what I learned from DCP-547

--- DCP-546.py
+++ DCP-546.py
@@ -1,4 +1,5 @@
 # This problem was asked by Google.
+# Difficuly: Medium
 
 # Given an array of integers, return a new array where each element in the new array 
 # is the number of smaller elements to the right of that element in the original input array.
@@ -18,3 +19,18 @@ for num in listA:
             count += 1
     listB.append(count)
 print(listB)
+
+#Note: After doing DCP 547, I realized this code could be optimized a little better:
+listB = []
+for j in range(len(listA)):
+    count = 0
+    for i in range(j + 1, len(listA)):
+        # print(listA[j], listA[i], i, count) #---DEBUG
+        if listA[i] < listA[j]:
+            count += 1
+    listB.append(count)
+print(listB)
+
+# Changes:
+# - Removed unecessary compount if clause by referring to list values by index instead of element
+
