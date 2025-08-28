4dc95d7e6310757b1500f898b24d2a15bed85dce
Tianna <75389500+UselessToaster@users.noreply.github.com>
2025-08-28 16:00:00 -0500

    Update DCP_2.java
    Added bonus part of problem and extra comments

--- DCP_2.java
+++ DCP_2.java
@@ -13,32 +13,32 @@ Follow-up: what if you can't use division?*/
 package Hard;
 class DCP_2 {
     public static void main(String [] args){
+        //given input
         int [] numbers = {1, 2, 3, 4, 5};
 
+        //Display solution using division
         int [] basic_result = basicSolution(numbers);
-        System.out.println("basic solution with division:\n");
+        System.out.println("basic solution with division:");
         for(int val : basic_result){
             System.out.print(val + " ");
         }
         System.out.println();
 
-        //Will finish with w/out division and improvement revisions in the morning 
-
-        /*int [] basic_result = basicSolution(numbers);
-        System.out.println("basic solution with division:\n");
-        for(int val : basic_result){
+        ///Display solution without division
+        int [] wOutDivResult = wOutDiv(numbers);
+        System.out.println("solution without division:");
+        for(int val : wOutDivResult){
             System.out.print(val + ", ");
         }
         System.out.println();
-        */
     }
+    //method with division
     public static int [] basicSolution(int[] inputArray){
         //get total product
         int product = 1;
         for(int num : inputArray){
             product *= num;
         }
-
         //divide given value to get solution
         int result;
         int [] outputArray = new int[inputArray.length];
@@ -46,7 +46,26 @@ class DCP_2 {
             result = product/inputArray[i];
             outputArray[i] = result;
         }
-
+        return outputArray;
+    }
+    //method without division
+    public static int[] wOutDiv(int[] inputArray){
+        //establish output array
+        int [] outputArray = new int [inputArray.length]; 
+        //iterate through input array
+        for(int i = 0; (i < inputArray.length); i++){
+            int product = 1;
+            int num = inputArray[i];
+            //#get product of all values not equal to outer loop iterator
+            for (int elem: inputArray){
+                if (elem != num){
+                    product *= elem;
+                }
+            }
+            //add product to new array
+            outputArray[i] = product;
+        }
         return outputArray;
     }
 }
+
